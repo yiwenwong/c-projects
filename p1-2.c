2e59367994c199e1051600c26b6bdbffc7429924
banyuan <banyuan@192.168.0.102>
2020-02-10 13:13:07 +0800

    homework

---
+++ p1-2.c
@@ -0,0 +1,9 @@
+//写一段程序，调用一次printf函数，显示下面内容。
+
+#include<stdio.h>
+
+int main(void)
+{
+    printf("天\n地\n人\n");
+    return 0;
+}
---
+++ p1-3.c
@@ -0,0 +1,8 @@
+//编写一段程序，调用一次printf函数，显示下面内容。
+#include<stdio.h>
+
+int main(void)
+{
+    printf("喂\n\n你好！\n再见。\n");
+    return 0;
+}
