96f31e02ce4fbc3395933063daf7b99ccb6e21d6
banyuan <banyuan@192.168.0.102>
2020-02-15 16:40:25 +0800

    homework

--- p5-10.c
+++ p5-10.c
@@ -1,5 +1,4 @@
-//创建一个函数，将4行3列矩阵a和3行4列矩阵b的乘积，存储在3行3列矩阵c中。
-
+//编写一段程序，求4行3列矩阵和3行4列矩阵的乘积。各构成元素的值从键盘输入。
 #include<stdio.h>
 void mat_mul(const int a[4][3],const int b[3][4],int c[3][3])
 {
@@ -12,19 +11,30 @@ void mat_mul(const int a[4][3],const int b[3][4],int c[3][3])
             {
                 c[i][j]=a[k][i]*b[j][k];
             }
-            
-             
-        }
-        
+        }   
     }
-    
-
 }
+
+
 int main(void)
 {   int i,j;
     int a[4][3]={0};
     int b[3][4]={0};
     int c[3][3]={0};
+    for (  i = 0; i < 4; i++)
+        {
+            for (j = 0; j < 3; j++)
+            {
+                scanf("%d",&a[i][j]);
+            }     
+        }
+        for (  i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 4; j++)
+            {
+                scanf("%d",&b[i][j]);
+            }     
+        }
 mat_mul(a, b ,c);
 for (  i = 0; i < 3; i++)
         {
