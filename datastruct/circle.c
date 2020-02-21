d907deba5185d992773cd87779dcfdd68713151d
banyuan <banyuan@192.168.0.102>
2020-02-21 22:54:57 +0800

    homework

--- datastruct/circle.c
+++ datastruct/circle.c
@@ -14,20 +14,51 @@ Node *initNode(Node *pre,int ele){
     return node;
 }
 void display(Node *p){
-    
+    Node *temp=p;
+    printf("有%d个元素",temp->ele);
+    while(temp->next){
+        temp=temp->next;
+        printf(" %d",temp->ele);
+    }
+    temp->next=p->next;//循环链表
+printf("\n");
 
 }
 
+void joseph(Node *p,int d,int SIZE){
+    Node *temp=p;
+    int count=0,count1=0;
+    while(count1<SIZE){
+         count++;
+        temp=temp->next;
+          
+        if(count==d-1){
+             count1++;
+         Node *del=temp->next;
+          printf(" %5d ",del->ele);
+         temp->next=del->next;
+         free(del);
+         del=NULL;
+         p->ele--;
+         count=0;
+        }  
+    }
+    printf("\n");
+}
 
 int main(void){
-    int SIZE,i;
+    int SIZE,i,dead;
     Node *head=(Node *)malloc(sizeof(Node));
-    Node *pre=head;
-    printf("元素个数为：");scanf("%d",&SIZE);
+     
+    printf("人数为：");scanf("%d",&SIZE);
     head->ele=SIZE;
+    printf("第几个去世：");scanf("%d",&dead);
+     Node *pre=head;
 
     for(i=0;i<SIZE;i++){
         pre=initNode(pre,i+1);
     }
-
+    display(head);
+     joseph(head,dead,SIZE);
+     return 0;
 }
